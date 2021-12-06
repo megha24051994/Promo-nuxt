2131bb4b710fd0bdbdddd0ece535415e16d65de7
unknown <m.jayakumar@ergotopia.de>
2021-12-06 10:49:25 +0100

    Fetch courses

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <Hero />
+    <hero />
     <section class="section">
       <div class="container">
         <h1 class="title">Featured Courses</h1>
         <div class="columns">
-          <div class="column is-one-quarter">
-            <!-- CARD-ITEM -->
-            <CourseCard />
-            <!-- CARD-ITEM-END -->
+          <!-- iterate columns with v-for and don't forget :key -->
+          <div
+            v-for="course in courses"
+            :key="course._id"
+            class="column is-one-quarter">
+            <!-- pass a course as a prop to course-card -->
+            <course-card :course="course"/>
           </div>
         </div>
       </div>
@@ -18,9 +21,7 @@
         <h1 class="title">Featured Articles</h1>
         <div class="columns">
           <div class="column is-one-quarter">
-            <!-- CARD-ITEM -->
             <blog-card />
-            <!-- CARD-ITEM-END -->
           </div>
         </div>
       </div>
@@ -29,20 +30,27 @@
 </template>
 
 <script>
-import  CourseCard  from '../components/CourseCard.vue'
-import  BlogCard  from '../components/CourseCard.vue'
-import  Hero  from '../components/shared/Hero.vue'
-
+import CourseCard from '~/components/CourseCard'
+import BlogCard from '~/components/BlogCard'
+import Hero from '~/components/shared/Hero'
+import { mapState } from 'vuex'
 export default {
   components: {
-    CourseCard,
-    BlogCard,
-    Hero
+    CourseCard, BlogCard, Hero
+  },
+  computed: {
+    ...mapState({
+      courses: state => state.course.items
+    })
+  },
+  async fetch({store}) {
+    await store.dispatch('course/fetchCourses')
   }
 }
 </script>
 
 <style scoped lang="scss">
+
   // Home page
   .links {
     padding-top: 15px;
